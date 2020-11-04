5f1ebfbfcb6aa3f25223871b2bcfcb38b7cf41e8
Klaus Rothenhäusler <rotk@zhaw.ch>
2020-11-04 22:53:59 +0100

    removed limit for most frequent words

--- context-atlas/get_cwb_vocabulary.py
+++ context-atlas/get_cwb_vocabulary.py
@@ -26,7 +26,6 @@ def get_vocab(
         )
 
         lstWords = []
-        lstLexLines = lstLexLines[100:]
         for lstWordInfo in lstLexLines:
             strWord = lstWordInfo[1]
             nFrequency = int(lstWordInfo[0])
