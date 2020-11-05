33f295ca12c7e86b2dc46ccd6142162f671f8189
Klaus Rothenhäusler <rotk@zhaw.ch>
2020-11-05 09:39:44 +0100

    debug

--- context-atlas/get_sentences_from_cwb_corpus.py
+++ context-atlas/get_sentences_from_cwb_corpus.py
@@ -119,7 +119,7 @@ def main(
     typer.secho("corpus processing complete", fg=typer.colors.MAGENTA, err=True)
     nLast = len(lstWords)-1
     print("[")
-    for i in enumerate(tqdm(lstWords)):
+    for i in enumerate(tqdm.tqdm(lstWords)):
         strWord = lstWords[i]
         lstSentenceData = vrtSentenceProvider.getSentenceDataForWord(
             strWord, nMaxCount=1000
